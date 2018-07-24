f56d8a4a4200d3417b1c3a2dbfe03a4a7738653c
ArrielleMCross <Arriellemyles@gmail.com>
2018-07-24 19:42:58 -0400

    1st css sheet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace code_projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Arrielle!");
+        }
+    }
+}
